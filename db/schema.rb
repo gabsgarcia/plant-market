d4a644234082604dc392e7f85f95322792669b87
gabriella garcia <gabirvgarcia@hotmail.com>
2021-11-02 18:17:22 -0300

    schema fixed with description

--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,10 @@ ActiveRecord::Schema.define(version: 2021_10_30_152436) do
   create_table "offers", force: :cascade do |t|
     t.string "title"
     t.string "category"
+    t.string "description"
     t.bigint "user_id"
     t.boolean "status"
+    t.integer "buyer_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
@@ -64,7 +66,7 @@ ActiveRecord::Schema.define(version: 2021_10_30_152436) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
