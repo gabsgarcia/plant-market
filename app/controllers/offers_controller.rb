646f68a0fc10222b1eeb40cc935c29141ee905a8
gabriella garcia <gabirvgarcia@hotmail.com>
2021-11-06 14:41:41 -0300

    markers no search

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -70,8 +70,24 @@ class OffersController < ApplicationController
   def search
     if params[:district].present? && params[:category].present?
       @offers = Offer.where(district: params[:district], category: params[:category])
+      @markers = @offers.geocoded.map do |offer|
+        {
+          lat: offer.latitude,
+          lng: offer.longitude,
+          info_window: render_to_string(partial: "info_window", locals: { offer: offer }),
+          image_url: helpers.asset_url('marker.png')
+        }
+      end
     else
       @offers = Offer.all
+      @markers = @offers.geocoded.map do |offer|
+        {
+          lat: offer.latitude,
+          lng: offer.longitude,
+          info_window: render_to_string(partial: "info_window", locals: { offer: offer }),
+          image_url: helpers.asset_url('marker.png')
+        }
+      end
     end
   end
 
